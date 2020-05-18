050111861d30a2d56017924e0a5ef74743fa0e9a
himatsukura <shuranui01kotonoha.sadafun@gmail.com>
2020-05-18 17:30:18 +0900

    ・コンテナに入ったあとの各種installを省略できるようにしました ・コンテナに入ったあと、すぐに「nodemon app」でサーバを起動できます

--- app2/app.js
+++ app2/app.js
@@ -1,11 +1,14 @@
 var express = require('express')
     app = express();
-
-
+var logger = require('morgan');
 
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.send('hello world!');
 })
+
+
+//middleware
+app.use(logger('dev'));
 
 app.listen(3000);
 console.log("server starting...");
