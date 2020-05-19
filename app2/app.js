f8315d52085285464c9161c053d70e5938f89202
makoshimbo <mako3125@icloud.com>
2020-05-19 17:54:24 +0900

    Cannot find module 'express'のエラーが出る状態

--- app2/app.js
+++ app2/app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+    app = express();
+    post = require('./routes/post');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+// put,deleteのオーバーライド用
+var connect        = require('connect');
+var methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engin', 'ejs');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));//putとgetを対応させるミドルウェア
 
+//app.use(express.logger('dev'));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//routing
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.get('/', post.index); //記事一覧表示
+/*
+app.get('/posts/new', post.new); //新規作成フォームを表示するルーティング
+app.post('/posts/create', post.create); //記事が生成されるルーティング
+app.get('/posts/:id', post.show); //詳細画面
+app.get('/posts/:id/edit', post.edit); //更新画面 編集フォームを表示
+app.put('/posts/:id', post.update); //編集フォームの投稿先
+app.delete('/posts/:id', post.destroy); //削除
+*/
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+
+
+app.listen(3000);
+console.log("server starting...")
+
---
+++ app2/routes/post.js
@@ -0,0 +1,4 @@
+//他のファイルから関数を呼び出す
+exports.index = function(req, res){
+    res.render('posts/index');
+};
