6ba01b0c75b2db70718244e24b586538b339e329
makoshimbo <mako3125@icloud.com>
2020-05-21 13:30:16 +0900

    Express入門第21回まで完了

--- app2/app.js
+++ app2/app.js
@@ -16,22 +16,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));//putとgetを対応させるミドルウェア
 
-//app.use(express.logger('dev'));
+
+//crsf対策
+var cookieParser   = require('cookie-parser');
+var expressSession = require('express-session');
+var csrf           = require('csurf');
+
+app.use(cookieParser());
+app.use(expressSession({secret: 'secret_key'}));
+app.use(csrf());
+
+app.use(function(req, res, next){
+    res.locals.csrftoken = req.csrfToken();
+    next();
+});
+
 app.use(logger('dev'));
+app.use(function(err, req, res, next){
+    res.send(err.message);
+
+});
+
 
 //routing
-
 app.get('/', post.index); //記事一覧表示
-/*
+app.get('/posts/:id([0-9]+)', post.show); //詳細画面
 app.get('/posts/new', post.new); //新規作成フォームを表示するルーティング
 app.post('/posts/create', post.create); //記事が生成されるルーティング
-app.get('/posts/:id', post.show); //詳細画面
 app.get('/posts/:id/edit', post.edit); //更新画面 編集フォームを表示
 app.put('/posts/:id', post.update); //編集フォームの投稿先
 app.delete('/posts/:id', post.destroy); //削除
-*/
-
-
 
 
 
--- app2/routes/post.js
+++ app2/routes/post.js
@@ -1,4 +1,56 @@
+var posts = [
+    {title: 'title0', body: 'body0'},
+    {title: 'title1', body: 'body1'},
+    {title: 'title2', body: 'body2'}
+
+];
+
 //他のファイルから関数を呼び出す
 exports.index = function(req, res){
-    res.render('posts/index');
+    res.render('posts/index' , {posts: posts}); //postsのデータをindexに受け渡す
 };
+
+exports.show = function(req, res){
+    res.render('posts/show' , {post: posts[req.params.id]}); //配列のデータn番目を渡す
+};
+
+exports.new = function(req, res){
+    res.render('posts/new'); //新規作成のフォームを作成　特に渡すデータはなし
+};
+
+exports.create = function(req, res){
+    var post = {
+        title: req.body.title,
+        body: req.body.body
+
+    };
+    posts.push(post);
+    res.redirect('/');
+};
+
+exports.edit = function(req, res){
+    res.render('posts/edit', {post: posts[req.params.id], id: req.params.id}); //編集しているIDを渡す
+};
+
+exports.update = function(req, res, next){
+    if(req.body.id !== req.params.id){
+        next(new Error('ID not valid'));
+    }else{
+        posts[req.body.id] = {
+            title: req.body.title,
+            body: req.body.body
+        };
+        res.redirect('/');
+    }
+
+    
+}; 
+
+exports.destroy = function(req, res, next){
+    if(req.body.id !== req.params.id){
+        next(new Error('ID not valid'));
+    }else{
+        posts.splice(req.body.id, 1);
+        res.redirect('/');
+    }
+}; 
